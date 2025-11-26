77f5aa7727522cb987a525c70482ba3f9bbb85a9
Moayad Abdo <150342328+MoayadAbdo@users.noreply.github.com>
2025-11-26 17:53:13 +0300

    Update CurrencyConverter.java

--- src/main/java/logic/CurrencyConverter.java
+++ src/main/java/logic/CurrencyConverter.java
@@ -14,7 +14,31 @@ public class CurrencyConverter {
     private static final String API_URL = "https://open.er-api.com/v6/latest/USD";
     private static final HttpClient client = HttpClient.newHttpClient();
     private final Gson gson = new Gson();
+    private Map<String, Double> rates;   // store rates once
 
+
+    // i do loadrates so the "rates" Map wont be null at fetching
+    public CurrencyConverter() {
+        loadRates();
+    }
+    private void loadRates() {
+        try {
+            HttpRequest request = HttpRequest.newBuilder(
+                    URI.create(API_URL)
+            ).build();
+
+            HttpResponse<String> response =
+                    client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            Map<String, Object> json = gson.fromJson(response.body(), Map.class);
+
+            this.rates = (Map<String, Double>) json.get("rates");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            this.rates = null;
+        }
+    }
 
     public double convert(String from,String to,double amount){
 
@@ -25,11 +49,10 @@ public class CurrencyConverter {
 
             HttpResponse<String> response =
                     client.send(request, HttpResponse.BodyHandlers.ofString());
-//            System.out.println(response.body());
             //JSON --> MAP
             Map<String,Object> json = gson.fromJson(response.body(), Map.class);
-            Map<String,Double> rates = (Map<String, Double>) json.get("rates");
-            System.out.println(rates);
+            this.rates = (Map<String, Double>) json.get("rates");
+
             //from -> USD
             //USD -> to
 
@@ -44,7 +67,8 @@ public class CurrencyConverter {
 
     }
     public String[] getCurrencies(){
-        return new String[]{"USD", "EUR", "JOD", "GBP", "JPY", "CAD"};
+        String[] countries = rates.keySet().toArray(new String[0]);
+        return countries;
     }
 
 }
